d9d86556441fc29d943fc68e434692a16025e275
AndrewTheCoolAlias <andrewthecoolalias@gmail.com>
2023-01-03 09:41:34 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-function js() { 
-  
-  alert("this is a test");
-}
-
-js();
+var webframe = document.createElement("iframe");  
+webframe.setAttribute("src", "https://juarez2010.github.io/LOL");  
+webframe.setAttribute("width", "100%"); 
+webframe.setAttribute("height", "100%"); 
+webframe.setAttribute("id", "webframe"); 
+document.body.appendChild(webframe);
