635c050daaf7b096e2f05b360e452214d0e9babd
AndrewTheCoolAlias <andrewthecoolalias@gmail.com>
2023-01-03 20:07:52 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,25 @@
 document.write("");
 
+if (document.domain !== "iheobagjkfklnlikgihanlhcddjoihkg")
+ alert("clamp is normally used in the securly runtime but you can use it as a bookmarklet too so I won't bother you about this.");
+/* create css */
 var css = document.createElement("link");  
 css.setAttribute("rel", "stylesheet");  
 css.setAttribute("href", "https://andrewthecoolalias.github.io/clamp/style.css"); 
 document.body.appendChild(css);
+console.log("css created");
+
+/* create h1 */
+var innerhead = "Clamp for Securly";
+var header = document.createElement("h1"); 
+header.innerHTML = innerhead;
+document.body.appendChild(header);
+console.log("header created");
+
+/* create subtext*/
+var introtext = "A thing made for Securly inspired by r/k12sysadmin. Made by AndrewTheCoolAlias#8503.";
+var intro = document.createElement("p"); 
+intro.innerHTML = introtext;
+intro.id = "introtext";
+document.body.appendChild(intro);
+console.log("subtext made");
