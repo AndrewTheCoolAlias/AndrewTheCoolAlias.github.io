b7eba6b096b1449959daf3ab1c1fc89631a33fe2
AndrewTheCoolAlias <andrewthecoolalias@gmail.com>
2023-01-03 16:10:26 -0800

    Update test.js

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ document.write("");
 
 var css = document.createElement("link");  
 css.setAttribute("rel", "stylesheet");  
-css.setAttribute("href", "https://andrewthecoolalias.github.io/style.css"); 
+css.setAttribute("href", "https://andrewthecoolalias.github.io/clamp/style.css"); 
 document.body.appendChild(css);
